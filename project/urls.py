f85094f4a29e56f61e4dd72ea1aa1dedef355e76
jakaprima <jakaprima123@gamil.com>
2018-02-23 14:38:36 +0700

    test

--- project/urls.py
+++ project/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^api/', include('settingweb.urls')),
+    url(r'^', include('settingweb.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
